e599bab9500a7d6681db10f619a15ed44064751c
vinothkumar95 <vinothkumar95@users.noreply.github.com>
2023-09-04 13:22:24 +0000

    Fix styling

--- src/Factory.php
+++ src/Factory.php
@@ -4,9 +4,9 @@ namespace Palpalani\BayRewards;
 
 use Palpalani\BayRewards\Resources\ActivityResource;
 use Palpalani\BayRewards\Resources\CreateActivityResource;
+use Palpalani\BayRewards\Resources\CustomerResource;
 use Palpalani\BayRewards\Resources\PointsResource;
 use Palpalani\BayRewards\Resources\StoreResource;
-use Palpalani\BayRewards\Resources\CustomerResource;
 use Saloon\Http\Connector;
 
 final class Factory extends Connector
--- src/Resources/CustomerResource.php
+++ src/Resources/CustomerResource.php
@@ -10,7 +10,7 @@ final class CustomerResource extends Resource
     /**
      * @return mixed|Store
      */
-    public function get(string $access_token,int $page =1,int $limit = 25,int $type = 0,string $sortBy = "", string $search = ""): mixed
+    public function get(string $access_token, int $page = 1, int $limit = 25, int $type = 0, string $sortBy = '', string $search = ''): mixed
     {
         return $this->connector->send(new GetStoreCustomerRequest($access_token, $page, $limit, $type, $sortBy, $search))->dto();
     }
