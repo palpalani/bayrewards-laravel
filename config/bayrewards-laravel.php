d387ddace9479fdb7e85887a95dd3aa71b5361ef
Palaniappan P <pal@targetbay.com>
2025-11-10 08:41:40 +0530

    fix: config

--- config/bayrewards-laravel.php
+++ config/bayrewards-laravel.php
@@ -2,7 +2,7 @@
 
 return [
     /**
-     * BayRewares rest API URL.
+     * BayRewards rest API URL.
      */
-    'bayrewards_base_url' => env('BAYREWARDS_BASE_URL'),
+    'bayrewards_base_url' => env('BAYREWARDS_BASE_URL', 'https://api.bayrewards.com'),
 ];
